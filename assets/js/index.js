8a97a729420398559c6868903f7e99785ad0e044
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2021-07-02 07:30:22 -0300

    Display of items on screen

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,4 +2,24 @@ import Api from './common/api.js';
 
 const api = new Api();
 
-console.log(await api.get('/index.php'));
+const items = await api.get('/list.php');
+
+const tbody = document.querySelector('tbody');
+let html = '';
+
+const formatter = new Intl.NumberFormat('pt-BR');
+
+for (const item of items) {
+    html += `
+        <tr>
+            <td>${item.id}</td>
+            <td>${item.name}</td>
+            <td>${item.description}</td>
+            <td>R$${formatter.format(item.price)}</td>
+            <td><button class="button is-warning is-fullwidth">Editar</button></td>
+            <td><button class="button is-danger is-fullwidth">Excluir</button></td>
+        </tr>
+    `;    
+}
+
+tbody.innerHTML = html;
