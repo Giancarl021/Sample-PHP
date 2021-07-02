c28bc22b3534fae7acfae6f274bd1e57d8804599
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2021-07-02 07:41:34 -0300

    Started button events

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,24 +2,44 @@ import Api from './common/api.js';
 
 const api = new Api();
 
-const items = await api.get('/list.php');
-
 const tbody = document.querySelector('tbody');
-let html = '';
 
-const formatter = new Intl.NumberFormat('pt-BR');
+await render();
 
-for (const item of items) {
-    html += `
+async function render() {
+    const items = await api.get('/list.php');
+
+    let html = '';
+
+    const formatter = new Intl.NumberFormat('pt-BR');
+
+    for (const item of items) {
+        html += `
         <tr>
             <td>${item.id}</td>
             <td>${item.name}</td>
             <td>${item.description}</td>
             <td>R$${formatter.format(item.price)}</td>
-            <td><button class="button is-warning is-fullwidth">Editar</button></td>
-            <td><button class="button is-danger is-fullwidth">Excluir</button></td>
+            <td><button class="button is-warning is-fullwidth edit">Editar</button></td>
+            <td><button class="button is-danger is-fullwidth remove">Excluir</button></td>
         </tr>
-    `;    
+    `;
+    }
+
+    tbody.innerHTML = html;
+
+    [ ...document.querySelectorAll('.remove') ]
+        .forEach(b => b.onclick = () => remove(b));
 }
 
-tbody.innerHTML = html;
+async function remove(element) {
+    const id = element
+        .parentElement
+        .parentElement
+        .querySelector('td:first-child')
+        .innerText;
+
+    console.log(id);
+
+    render();
+}
