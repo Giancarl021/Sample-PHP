60be14356de0116da664c25320cf829e2c0edcdc
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2021-07-02 20:53:37 -0300

    Edit endpoint

---
+++ api/edit.php
@@ -0,0 +1,34 @@
+<?php
+require "common/database.php";
+require "common/request.php";
+require "common/response.php";
+
+$request = new Request();
+$response = new Response();
+
+try {
+    $params = $request->getBodyParams(["id", "name", "description", "price"]);
+} catch (Exception $e) {
+    $response->error($e->getMessage(), 400);
+    die;
+}
+
+try {
+    $mysql
+        ->prepare("UPDATE Product SET name=?, description=?, price=? WHERE id=?")
+        ->run([
+            $params["name"],
+            $params["description"],
+            $params["price"],
+            $params["id"]
+        ]);
+} catch (Exception $e) {
+    $response->error("Database error: " . $e->getMessage());
+    die;
+}
+
+$response
+    ->status(200)
+    ->send();
+
+$mysql->close();
